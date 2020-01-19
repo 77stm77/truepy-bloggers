438e6ed6f55df3f4461addbe7246dc3bcc02a2b7
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-01-19 22:02:08 +0200

    g

--- truepy_bloggers/settings.py
+++ truepy_bloggers/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'truepy_bloggers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
